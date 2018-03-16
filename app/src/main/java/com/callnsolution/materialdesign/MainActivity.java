7e7d928d7b8778452b7fe5579f5ec19218d292d9
Shahed Mohammad Hridoy <shahed_hridoy@outlook.com>
2018-03-16 16:37:17 +0600

    Toolbar Added

--- app/src/main/java/com/callnsolution/materialdesign/MainActivity.java
+++ app/src/main/java/com/callnsolution/materialdesign/MainActivity.java
@@ -2,6 +2,7 @@ package com.callnsolution.materialdesign;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -11,10 +12,16 @@ import java.util.zip.Inflater;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
     }
 
     @Override
