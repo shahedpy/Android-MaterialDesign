8185b7c8ebe2f68e7f6fc3575d2ece6938a86940
Shahed Mohammad Hridoy <shahed_hridoy@outlook.com>
2018-03-20 20:22:37 +0600

    Action Button Added to Toolbar

--- app/src/main/java/com/callnsolution/materialdesign/SubActivity.java
+++ app/src/main/java/com/callnsolution/materialdesign/SubActivity.java
@@ -1,49 +1,39 @@
 package com.callnsolution.materialdesign;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class SubActivity extends AppCompatActivity {
 
     Toolbar toolbar;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_sub);
 
         toolbar = findViewById(R.id.app_bar);
         setSupportActionBar(toolbar);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setHomeButtonEnabled(true);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-
-        int id = item.getItemId();
-
-        if (id == R.id.action_settings) {
-            Toast.makeText(this, "Settings Pressed", Toast.LENGTH_LONG).show();
-        }
-
-        if (id == R.id.navigate) {
-            startActivity(new Intent(this, SubActivity.class));
-        }
-
         return super.onOptionsItemSelected(item);
-
     }
 }
